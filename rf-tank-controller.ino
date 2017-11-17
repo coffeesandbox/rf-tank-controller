ee5488e871f64b1199bfd3a6b017acb4f79b9b3b
Sergiu Falcusan <sergiu@falcusan.ro>
2017-11-18 01:54:45 +0200

    tank forward movemnt and direction changes

--- rf-tank-controller.ino
+++ rf-tank-controller.ino
@@ -17,6 +17,10 @@ typedef struct stateUpdate {
  
 StateUpdate stateUpdate;
 
+int leftStick = A0;   // Down-Up
+int rightStick = A1;  // Left-Right
+int controlPot = A2;  // RightMotor Sync (might be needed to sync motor speed)
+
 void setup() {  
   // Initialize serial communication
   Serial.begin(57600);
@@ -30,22 +34,67 @@ void setup() {
   radio.startListening();
   radio.printDetails();
 
-  // Random seed
-  randomSeed(analogRead(0));
+  // Initialize analog inputs
+  pinMode(leftStick,   INPUT);
+  pinMode(rightStick,   INPUT);
+  pinMode(controlPot,  INPUT);
 
   Serial.println("Initialized");
 }
 
+void computeMotorsSpeed() {
+  int leftStickValue = analogRead(leftStick);
+
+  if (leftStickValue < 490) {
+    stateUpdate.motorRight = map(leftStickValue, 500, 350, 0, 100); //86->55
+    stateUpdate.motorLeft = map(leftStickValue, 500, 350, 0, 100); 
+  } else if (leftStickValue > 510) {
+    stateUpdate.motorRight = map(leftStickValue, 660, 500, 0, 100);
+    stateUpdate.motorLeft = map(leftStickValue, 660, 500, 0, 100); 
+  } else {
+    stateUpdate.motorRight = 86;
+    stateUpdate.motorLeft = 86;
+  }
+
+  int rightStickValue = analogRead(rightStick); 
+  int directionAdjustment = map(rightStickValue, 330, 680, 0, 100);
+
+  printf("1. L [%d] R [%d] A [%d] \n",stateUpdate.motorLeft, stateUpdate.motorRight, directionAdjustment);
+
+  if (directionAdjustment >= 48) {
+    directionAdjustment = map(directionAdjustment, 48, 100, 0, 100);
+    stateUpdate.motorLeft = stateUpdate.motorLeft - (stateUpdate.motorLeft * directionAdjustment) / 100;
+  } else if (directionAdjustment <= 44) {
+    directionAdjustment = map(directionAdjustment, 0, 44, 100, 0);
+    stateUpdate.motorRight = stateUpdate.motorRight - ((stateUpdate.motorRight * directionAdjustment) / 100);
+  }
+
+  printf("2. L [%d] R [%d] A [%d] \n",stateUpdate.motorLeft, stateUpdate.motorRight, directionAdjustment);
+
+  if (leftStickValue < 490) {
+    stateUpdate.motorRight = map(stateUpdate.motorRight, 0, 100, 86, 55); //86->55
+    stateUpdate.motorLeft = map(stateUpdate.motorLeft, 0, 100, 86, 55); 
+  } else if (leftStickValue > 510) {
+    stateUpdate.motorRight = map(stateUpdate.motorRight, 0, 100, 120, 100);
+    stateUpdate.motorLeft = map(stateUpdate.motorLeft, 0, 100, 120, 100); 
+  } else {
+    stateUpdate.motorRight = 86;
+    stateUpdate.motorLeft = 86;
+  }
+
+  printf("3. L [%d] R [%d] A [%d] \n",stateUpdate.motorLeft, stateUpdate.motorRight, directionAdjustment);
+}
+
 void loop() {    
-  stateUpdate.motorLeft = random(60, 77);
-  stateUpdate.motorRight = random(60, 77);
+  int controlPotValue = analogRead(controlPot);
+
+  computeMotorsSpeed();
   
   radio.stopListening();
   if (radio.write(&stateUpdate, sizeof(stateUpdate))) {
-      printf("State update: Left [%d], Right [%d]\n", stateUpdate.motorLeft, stateUpdate.motorRight);
+      printf("State update: Left [%d], Right [%d], ControlPot [%d]\n", stateUpdate.motorLeft, stateUpdate.motorRight, controlPotValue);
   }
-
   radio.startListening();
   
-  delay(5000);
+  delay(50);
 }
